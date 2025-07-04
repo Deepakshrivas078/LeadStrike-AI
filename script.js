9f4b491d5c40874d0b4c70e43aeab839ef7882b3
Deepakshrivas078 <dshrivas237@gmail.com>
2025-07-04 14:04:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  auth.signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      document.getElementById("message").innerText = "Login successful!";
-      // window.location.href = "dashboard.html"; // Future redirect
-    })
-    .catch((error) => {
-      document.getElementById("message").innerText = "Error: " + error.message;
-    });
-}
-
-function signup() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  auth.createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      document.getElementById("message").innerText = "Account created!";
-    })
-    .catch((error) => {
-      document.getElementById("message").innerText = "Error: " + error.message;
-    });
-}
+<!-- ADD THIS TO TOP OF script.js -->
+<script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.6.8/firebase-auth.js"></script>
